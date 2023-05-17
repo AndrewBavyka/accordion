a620aaf10ff72c306de99d795be93daea1822d9e
AndrewBavyka <abavyka@list.ru>
2023-05-18 00:28:04 +0500

    готовый унверсальный акордеон

--- script/script.js
+++ script/script.js
@@ -1,46 +1,27 @@
 'use strict'
 
-const input = document.querySelector('input'),
-    guess = document.querySelector('.guess'),
-    checkBtn = document.querySelector('button'),
-    chances = document.querySelector('.chances');
+const menuTitle = document.querySelectorAll('.footer-menu__title'),
+    subMenuList = document.querySelectorAll('.footer-submenu__list');
 
+function accordion(menuTitle, menuList) {
 
-window.addEventListener('DOMContentLoaded', () => {
-    input.focus();
+    menuTitle.forEach((item, index) => {
+        item.addEventListener('click', (e) => {
+            if (e.target) {
+                menuList.forEach((menuListItem, i) => {
+                    if (index == i || menuListItem.classList.contains('active')) {
+                        menuListItem.classList.toggle('active');
 
-    let givenNumver = 0;
-
-    function generateRandomNum(min, max) {
-        return givenNumver += Math.floor(Math.random() * (max - min) + min);
-    }
-    generateRandomNum(1, 100);
-    console.log(givenNumver);
-
-
-    let chanceCount = 10;
-
-    function chanceDecrease(chance) {
-        chance.innerHTML = chanceCount--;
-    };
-
-    chanceDecrease(chances);
-
-    function checkedNumber() {
-        checkBtn.addEventListener('click', (e) => {
-            let inputValue = Number(input.value);
-
-            if (e.target && inputValue === givenNumver) {
-                console.log('Вы угадали число!');
-            } else if (inputValue === 0) {
-                console.log('Введите число!');
-            } else {
-                console.log('Вы не угадали число!');
-                chanceDecrease(chances);
+                        if (menuListItem.style.maxHeight) {
+                            menuListItem.style.maxHeight = null;
+                        } else {
+                            menuListItem.style.maxHeight = menuListItem.scrollHeight + "px";
+                        }
+                    }
+                })
             }
         });
-    };
+    });
+};
 
-    checkedNumber();
-
-});
+accordion(menuTitle, subMenuList);
